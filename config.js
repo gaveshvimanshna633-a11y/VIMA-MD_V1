98907d51efb6f79b65cd175c837405f12a233818
gaveshvimanshna633-a11y <gaveshvimanshna633@gmail.com>
2026-04-28 15:41:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=471fa0153e6c68cc",
-    ALIVE_IMG : process.env.ALIVE_IMG  || "",
+    ALIVE_IMG : process.env.ALIVE_IMG  || "https://raw.githubusercontent.com/gaveshvimanshna633-a11y/Vima-md/refs/heads/main/Image/169468-war%20po4.jpg",
     ALIVE_MSG : process.env.ALIVE_MSG  || "pakaya",
 };
