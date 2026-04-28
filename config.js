e655b53176b6001882fdb2e6da2e8f8de6914c84
gaveshvimanshna633-a11y <gaveshvimanshna633@gmail.com>
2026-04-28 15:28:43 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=471fa0153e6c68cc"
+};
